6ffbb8074c7670bf3d4f7be1febd1ebb101bd86b
User <User@YD03-10>
2019-09-16 14:11:57 +0900

    test

--- HelloWorld/src/Hello/Exercise4I7.java
+++ HelloWorld/src/Hello/Exercise4I7.java
@@ -7,7 +7,7 @@ public class Exercise4I7 {
 		
 		int balance = 0; // 총 잔고를 선언 해준다.
 		
-		Scanner input = new Scanner(System.in); //스캐너 함수를 사용하여 인풋을 넣어주는 함수를 작성한다. 
+		Scanner scanner = new Scanner(System.in); //스캐너 함수를 사용하여 인풋을 넣어주는 함수를 작성한다. 
 		
 		while(run) { //while 문을 통해 밑의 출력문들을 출력해준다.
 			System.out.println("---------------------------");
@@ -15,15 +15,15 @@ public class Exercise4I7 {
 			System.out.println("---------------------------");
 			System.out.print("선택> ");
 			
-			int a = input.nextInt(); // 인트 a의 값은 인풋 값을 측정한다.
+			int a = scanner.nextInt(); // 인트 a의 값은 인풋 값을 측정한다.
 			
 			if(a == 1) { // if문으로 a는 1과 같다는 선언을 해주면 1을 넣었을때 밑의 출력문이 나오고  예금액이 출력된다.
 				System.out.print("예금액> "); 
-				balance += input.nextInt();// 출력된 예금액의 금액을 대입연산자 += 를 통해 밸런스에 금액을 증가해준다.
+				balance += scanner.nextInt();// 출력된 예금액의 금액을 대입연산자 += 를 통해 밸런스에 금액을 증가해준다.
 				System.out.println("잔액> " + balance);
 			} else if(a == 2) { // 2를 a와 같다는 선언을 통해 2를 입력하면
 				System.out.print("출금액> "); // 출금액이 나오고 
-				balance -= input.nextInt(); // 밸런스에 있는 숫자를 인풋받은 숫자를 -= 대입연산자를 통해 금액을 가감해준다.
+				balance -= scanner.nextInt(); // 밸런스에 있는 숫자를 인풋받은 숫자를 -= 대입연산자를 통해 금액을 가감해준다.
 				System.out.println("잔액> " + balance);
 			} else if(a == 3) { // 3을 a와 같다는 선언을 통해 3을 입력하면
 				System.out.println("잔고>"+balance); // 현재 밸런스를 표시해준다.
