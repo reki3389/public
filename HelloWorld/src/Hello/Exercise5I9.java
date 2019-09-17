4896540646dc383429c9ed1499e10a0b5641dfbf
Kwon <Kwon@DESKTOP-D3CCSNF>
2019-09-17 21:39:20 +0900

    test

--- HelloWorld/src/Hello/Exercise5I9.java
+++ HelloWorld/src/Hello/Exercise5I9.java
@@ -13,24 +13,35 @@ public class Exercise5I9 {
 			System.out.println("-----------------------------------------");
 			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
 			System.out.println("-----------------------------------------");
-			System.out.println("선택> ");
+			System.out.print("선택> ");
 			
 			int selectno = scanner.nextInt();
 			
 			if(selectno == 1) {
-				studentnum += scanner.nextInt();
-				System.out.println("학생수> "+studentnum);
+				System.out.print("학생수> ");
+				studentnum = scanner.nextInt();
 				scores = new int[studentnum];
+				System.out.println("학생수 : "+ studentnum);
 			} else if(selectno == 2) {
 				for(int i = 0; i<studentnum; i++) {
+					System.out.print("score["+i+"]:");
 					scores[i] = scanner.nextInt();
-				}
+					}
 			} else if(selectno == 3) {
-				for(int i = 0; i<studentnum; i++)
-				System.out.println(scores[i]);
+				for(int i = 0; i<studentnum; i++) {
+				System.out.println("score["+i+"]:"+scores[i]);
+				}
 			} else if(selectno == 4) {
-				int max;
-				max = (scores[i] > studentnum ? 
+				int max = 0;
+				int sum = 0;
+				double avg = 0;
+				for(int i = 0; i<scores.length; i++) {
+					max = (max < scores[i]) ? scores[i] : max;
+					sum += scores[i];
+				}
+					avg =  (double) sum / studentnum ;
+					System.out.println("제일 높은 점수 :"+max);
+					System.out.println("평균 점수 :"+avg);
 			} else if(selectno == 5) {
 				run = false;
 			}
