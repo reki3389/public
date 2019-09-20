0fae17e119afca1009ce0f950d9c5d17227276c3
Kwon <Kwon@DESKTOP-D3CCSNF>
2019-09-20 19:44:56 +0900

    test

--- HelloWorld/src/Hello/Test.java
+++ HelloWorld/src/Hello/Test.java
@@ -1,13 +1,9 @@
 package Hello;
 
 public class Test {
-public static void main(String[] args) {
-		int a = 0;
-			for(int i=1; i<=10; i++) {
-				if(i%2 != 0) {
-				a+=i;
-				}
-			}
-		System.out.println("1부터 10까지 홀수의 합은 : "+a);
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
 	}
+
 }
