2751a6f4bab364cc5cf8e050d1001f6c24435d04
Kwon <Kwon@DESKTOP-D3CCSNF>
2019-10-01 21:28:57 +0900

    test

--- HelloWorld/src/Hello/test4.java
+++ HelloWorld/src/Hello/test4.java
@@ -1,17 +1,21 @@
 package Hello;
 
-import java.util.Scanner;
-
 public class test4 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = 0;
-		int[] a = new int[n];
-		int sum = 0;
-		for (int i = 0; i <= a.length; i++) {
-			sum += i;
-
-		}
-		System.out.println(sum);
+		test5 mycom = new test5();
+		
+		int[] values1 = {1, 2, 3};
+		int result1 = mycom.sum1(values1);
+		System.out.println(result1);
+		
+		int result2 = mycom.sum1(new int[] {1,2,3});
+		System.out.println(result2);
+		
+		int result3 = mycom.sum2(1, 2, 3);
+		System.out.println(result3);
+		
+		int result4 = mycom.sum2(1, 2, 3, 4, 5);
+		System.out.println(result4);
+		
 	}
 }
--- HelloWorld/src/com/yedam/generic/Utils.java
+++ HelloWorld/src/com/yedam/generic/Utils.java
@@ -1,5 +1,20 @@
 package com.yedam.generic;
 
 public class Utils {
+//	public static <K, V> V getValue(Pair<K, V> p, K k) {
+//		if(p.getKey() == k) {
+//			return p.getValue();
+//		}else {
+//			return null;
+//		}
+//	}
+	
+	public static <P extends Pair<K, V>, K, V> V getValue(P p, K k) {
+	if(p.getKey() == k) {
+		return p.getValue();
+	}else {
+		return null;
+	}
+}
 	
 }
