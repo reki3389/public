97713c114a1d880c2fd67e6d0c9c81bb300c48d7
User <User@YD03-10>
2019-10-02 08:54:48 +0900

    test

--- HelloWorld/src/com/yedam/generic/Utils.java
+++ HelloWorld/src/com/yedam/generic/Utils.java
@@ -1,5 +1,11 @@
 package com.yedam.generic;
 
 public class Utils {
-	
+	public static <K, V> Pair<K, V> pairing(K k, V v) {
+		return null;	
+	}
+	public static <V extends Pair> int compare(V k1, V v1) {
+		int v1 = k1;
+		
+	}
 }
